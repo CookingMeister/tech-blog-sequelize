a06e5da334c13db9806f6abdc369de0d3ed5628d
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-02-06 11:45:25 -0400

    create connection.js, .env file

---
+++ config/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  async (email, password, done) => {
+    try {
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = passport;
