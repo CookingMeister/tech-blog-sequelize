d00e15c44be7a41e6b5b5f39d19806619503c7c9
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-02-08 15:25:30 -0400

    add working home, register, login and posts routes using ejs, stylesheets active

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 // const LocalStrategy = require('passport-local').Strategy; /////  not needed for now
 const sequelize = require('./config/connection.js');
 const routes = require('./routes');
+// const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.set('view engine', 'ejs');
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static((__dirname + '/public/styles')));
 
 // Session setup
 const sessionStore = new SequelizeStore({ db: sequelize });
