d0252ae9ed27d79f063e5a84d24c90c64ae7d0b3
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-02-24 14:57:50 -0400

    fix register logic to create new user if not exist, hash password, login user, save session, redirect to dashboard, working

--- routes/api/register-route.js
+++ routes/api/register-route.js
@@ -11,28 +11,49 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
 
-  try {
+  try {   // Look for existing user
     const existingUser = await User.findOne({ where: { username } });
-
     existingUser
       ? res.status(400).json({ error: 'Username already in use' })
-      : bcrypt.hash(password, saltRounds).then((hashedPassword) => {
-          User.create({
-            username,
-            password: hashedPassword,
-          }).then((newUser) => {
-            req.session.loggedIn = true;
-            req.login(newUser, (err) => {
-              if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Internal Server Error' });
-              }
-              console.log('New User created and logged in');
-              return res.json(newUser);
-            });
+      : bcrypt
+          .hash(password, saltRounds)   // Hash password
+          .then((hashedPassword) => {
+            User.create({               // Create new user
+              username,
+              password: hashedPassword,
+            })
+              .then((newUser) => {
+                req.login(newUser, (err) => { // Log in new user
+                  if (err) {
+                    console.error(err);
+                    return res
+                      .status(500)
+                      .json({ error: 'Error logging in new user' });
+                  }
+                  req.session.userId = newUser.id;
+                  req.session.loggedIn = true; // Set session loggedIn to true
+                  req.session.save((err) => {
+                    if (err) {
+                      console.error(err);
+                      return res
+                        .status(500)
+                        .json({ error: 'Error saving user session' });
+                    }
+                    console.log(newUser, 'New User created and logged in');
+                    return res.redirect('/api/dashboard');  
+                  });
+                });
+              })  // Error handling for User.create()
+              .catch((error) => {
+                console.error(error);
+                return res.status(500).json({ error: 'Error creating new user' });
+              });
+          })  // Error handling for bcrypt.hash()
+          .catch((error) => {
+            console.error(error);
+            return res.status(500).json({ error: 'Error hashing password' });
           });
-        });
-  } catch (error) {
+  } catch (error) { // Error handling for try User.findOne()
     console.error(error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
