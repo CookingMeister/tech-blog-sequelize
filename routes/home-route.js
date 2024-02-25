c2c222613a70a00b758028933d5d05873ea72fe5
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-02-25 13:49:21 -0400

    add functionality on homepage to display author of posts based on where post.userId equals user.id, AKA get author of all posts correctly, working

--- routes/home-route.js
+++ routes/home-route.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
-const { Post } = require('./../models');
+const { Post, User } = require('./../models');
 
 // Route to render the home page using EJS
 router.get('/', async (req, res) => {
   try {
   // Fetch all posts from the database
   const posts =  await Post.findAll();
+  const users = await User.findAll();
   // Render dashboard with posts array
-  res.render('home1', { posts, user: req.user });
+  res.render('home1', { posts, users, user: req.user });
     // res.render('home1');
   } catch (error) {
     // Handle errors
