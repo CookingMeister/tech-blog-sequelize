8e40c2b37600554a1d3a0a7f6be80aabcd93e67f
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-03-02 09:43:33 -0400

    add bootstrap alert condition to delete user button, working

--- public/js/editUser.js
+++ public/js/editUser.js
@@ -1,5 +1,6 @@
 let id;
 
+//  Populate Modal on click
 document.querySelectorAll('.editDetails').forEach((item) => {
   item.addEventListener('click', (event) => {
     id = event.target.getAttribute('data-id');
@@ -15,60 +16,76 @@ document.querySelectorAll('.editDetails').forEach((item) => {
   });
 });
 
+//  Update username
 document.querySelector('.editDetails').addEventListener('click', () => {
   document.querySelector('.update-user-btn').addEventListener('click', () => {
-    const username = document.querySelector(
-      '.form-control'
-    ).value;
-    
+    const username = document.querySelector('.form-control').value;
+
     axios
-      .post('/api/user/' + id, { username } )
+      .post('/api/user/' + id, { username })
       .then((response) => {
         // Close the modal
         let editModal = bootstrap.Modal.getInstance(
           document.getElementById('editModal')
         );
         editModal.hide();
-        window.location.reload();        
+        window.location.reload();
       })
       .catch((error) => {
         // Error handling
         console.log(error);
-        
       });
   });
 });
 
-  document.querySelector('.delete-btn').addEventListener('click', () => {
+//  Delete user profile
+document.querySelector('.delete-btn').addEventListener('click', () => {
   document.querySelector('#modalContent').value = id;
-  axios
-    .delete('/api/user/' + id)
-    .then((response) => {
-      // Close the modal
-      let editModal = bootstrap.Modal.getInstance(
-        document.getElementById('editModal')
-      );
-      editModal.hide();
-      window.location.replace('/');
-    })
-    .catch((error) => {
-      // Error handling
-      console.log(error.response);
-    });
+
+  const alertMessage = document.getElementById('alertMessage');
+  alertMessage.classList.remove('d-none'); // Show the alert
+  // Yes prompt to Delete
+  document.querySelector('.yesClicked').addEventListener('click', () => {
+    console.log('yes clicked');
+    alertMessage.classList.add('d-none'); // Hide the alert
+    const deletePrompt = axios.delete('/api/user/' + id);
+    deletePrompt
+      .then((response) => {
+        // Close the modal
+        let editModal = bootstrap.Modal.getInstance(
+          document.getElementById('editModal')
+        );
+        editModal.hide();
+        window.location.replace('/');
+      })
+      .catch((error) => {
+        // Error handling
+        console.log(error.response);
+      });
+  });
+  // No prompt to not Delete
+  document.querySelector('.closeDelete').addEventListener('click', () => {
+    console.log('no clicked');
+    alertMessage.classList.add('d-none'); // Hide the alert
+    let editModal = bootstrap.Modal.getInstance(
+      document.getElementById('editModal')
+    );
+    editModal.hide();
+  });
 });
 
 // Logout button listener
 const logout = document.querySelector('.logout-btn');
 if (logout) {
-    logout.addEventListener('click', () => {
-      console.log('logout clicked');
-      const logoutPrompt = axios.get('/api/logout');
-      logoutPrompt
-        .then(function (response) {
-          window.location.replace('/');
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    });
-   };
+  logout.addEventListener('click', () => {
+    console.log('logout clicked');
+    const logoutPrompt = axios.get('/api/logout');
+    logoutPrompt
+      .then(function (response) {
+        window.location.replace('/');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+}
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const postRoutes = require('./post-route');
+// const postRoutes = require('./post-route');
 const userRoutes = require('./user-profile-route');
 const registerRoutes = require('./register-route');
 const loginRoutes = require('./login-route');
@@ -8,7 +8,7 @@ const dashboardRoutes = require('./dashboard-route');
 const commentRoutes = require('./comment-route');
 
 router.use('/register', registerRoutes);
-router.use('/posts', postRoutes);
+// router.use('/posts', postRoutes);
 router.use('/user', userRoutes);
 router.use('/login', loginRoutes);
 router.use('/logout', logoutRoutes);
